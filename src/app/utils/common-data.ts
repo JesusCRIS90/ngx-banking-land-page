b92f40baaba72e4677b4284e696c765b329eadc9
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-09-20 17:51:18 +0200

    [MOD] Externalization Assets Loading

--- src/app/utils/common-data.ts
+++ src/app/utils/common-data.ts
@@ -1,4 +1,9 @@
-export const SVGs_PATH = '/assets/sprite-sheet/icons-sheet.svg';
-export const DATA_PATH = '/assets/data/app-data.json';
+// export const SVGs_PATH = '/assets/sprite-sheet/icons-sheet.svg';
+// export const DATA_PATH = '/assets/data/app-data.json';
+
+export const SVGs_PATH = 'https://raw.githubusercontent.com/JesusCRIS90/jc-risquez-cdn/refs/heads/main/a-bank-for-life/icons-sheet.svg';
+export const DATA_PATH = 'https://raw.githubusercontent.com/JesusCRIS90/jc-risquez-cdn/refs/heads/main/a-bank-for-life/data/app-data.json';
 
 export const DATA_KEY = "AppKey"; 
+
+
